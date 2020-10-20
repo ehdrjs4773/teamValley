48432f32350a95aac859587239658dd2648859fe
Nia Kim <niakim63@gmail.com>
2020-10-20 21:26:39 +0900

    제작 창 제작아이템 정보 띄우기

--- 스타듀밸리 맵툴/inventoryCraft.h
+++ 스타듀밸리 맵툴/inventoryCraft.h
@@ -12,6 +12,21 @@ public:
 	tagItem _CraftItem;
 	vector<tagItem> _vCraftItem;
 
+	int _isWood;
+	int _isRock;
+	int _isIron;
+	int _isCopper;
+
+	bool _isTemp3;
+	bool _isTemp4;
+
+	bool _isWoodOk;
+	bool _isRockOk;
+	bool _isIronOk;
+	bool _isCopperOk;
+
+
+
 public :
 	void init();
 	void update();
