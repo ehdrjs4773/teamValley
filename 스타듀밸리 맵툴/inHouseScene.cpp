5d7830ff571fa8f8e3d0f2ff26e977c0c064c978
Nia Kim <niakim63@gmail.com>
2020-10-27 22:40:25 +0900

    집안에서도 인벤토리 켰을때 안움직이게 수정

--- 스타듀밸리 맵툴/inHouseScene.cpp
+++ 스타듀밸리 맵툴/inHouseScene.cpp
@@ -40,6 +40,9 @@ void inHouseScene::update()
 	}
 
 	PLAYER->update();
+
+
+
 	CAMERAMANAGER->cameraMove(PLAYER->getCenterX(), PLAYER->getCenterY());
 	playerMove();
 	PLAYER->playerAnimation();
@@ -93,72 +96,79 @@ void inHouseScene::render()
 
 void inHouseScene::playerMove()
 {
-	if (INPUT->GetKey('W'))
+	if (!PLAYER->getIsShowInventory()) //인벤토리가 안떠있을때 
 	{
-		if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX(), PLAYER->getCenterY()) != RGB(255, 0, 0))
-		
+		if (INPUT->GetKey('W'))
 		{
-			if (!SOUNDMANAGER->isPlaySound("movewood"))
+			if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX(), PLAYER->getCenterY()) != RGB(255, 0, 0))
+
 			{
-				SOUNDMANAGER->play("movewood", 0.2f);
-			}
-			PLAYER->setDirection(UP);
-			PLAYER->setState(RUN);
-			PLAYER->setCenterY(PLAYER->getCenterY() - PLAYER->getSpeed());
-		}
-	}
-	if (INPUT->GetKey('S'))
-	{
-		if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX(), PLAYER->getCenterY() + 16) != RGB(255, 0, 0))
-		{
-			if (!SOUNDMANAGER->isPlaySound("movewood"))
-			{
-				SOUNDMANAGER->play("movewood", 0.2f);
-			}
-			PLAYER->setDirection(DOWN);
-			PLAYER->setState(RUN);
-			PLAYER->setCenterY(PLAYER->getCenterY() + PLAYER->getSpeed());
-		}
-		if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX(), PLAYER->getCenterY() + 16) == RGB(0, 0, 255))
-		{
-			if (!SWITCHMANAGER->getFade())
-			{
-				SWITCHMANAGER->changeScene("인게임화면");
-				SWITCHMANAGER->startFade(416.0f, 912.0f); // 플레이어가 인게임에서 호출될 장소 
+				if (!SOUNDMANAGER->isPlaySound("movewood"))
+				{
+					SOUNDMANAGER->play("movewood", 0.2f);
+				}
+				PLAYER->setDirection(UP);
+				PLAYER->setState(RUN);
+				PLAYER->setCenterY(PLAYER->getCenterY() - PLAYER->getSpeed());
 			}
 		}
-	}
-	if (INPUT->GetKey('A'))
-	{
-		if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX() - 8, PLAYER->getCenterY() + 8) != RGB(255, 0, 0))
+		if (INPUT->GetKey('S'))
 		{
-			if (!SOUNDMANAGER->isPlaySound("movewood"))
+			if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX(), PLAYER->getCenterY() + 16) != RGB(255, 0, 0))
 			{
-				SOUNDMANAGER->play("movewood", 0.2f);
+				if (!SOUNDMANAGER->isPlaySound("movewood"))
+				{
+					SOUNDMANAGER->play("movewood", 0.2f);
+				}
+				PLAYER->setDirection(DOWN);
+				PLAYER->setState(RUN);
+				PLAYER->setCenterY(PLAYER->getCenterY() + PLAYER->getSpeed());
 			}
-			PLAYER->setDirection(LEFT);
-			PLAYER->setState(RUN);
-			PLAYER->setCenterX(PLAYER->getCenterX() - PLAYER->getSpeed());
+			if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX(), PLAYER->getCenterY() + 16) == RGB(0, 0, 255))
+			{
+				if (!SWITCHMANAGER->getFade())
+				{
+					SWITCHMANAGER->changeScene("인게임화면");
+					SWITCHMANAGER->startFade(416.0f, 912.0f); // 플레이어가 인게임에서 호출될 장소 
+				}
+			}
 		}
-	}
-	if (INPUT->GetKey('D'))
-	{
-		if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX() + 8, PLAYER->getCenterY() + 8) != RGB(255, 0, 0))
+		if (INPUT->GetKey('A'))
 		{
-			if (!SOUNDMANAGER->isPlaySound("movewood"))
+			if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX() - 8, PLAYER->getCenterY() + 8) != RGB(255, 0, 0))
 			{
-				SOUNDMANAGER->play("movewood", 0.2f);
+				if (!SOUNDMANAGER->isPlaySound("movewood"))
+				{
+					SOUNDMANAGER->play("movewood", 0.2f);
+				}
+				PLAYER->setDirection(LEFT);
+				PLAYER->setState(RUN);
+				PLAYER->setCenterX(PLAYER->getCenterX() - PLAYER->getSpeed());
 			}
-			PLAYER->setDirection(RIGHT);
-			PLAYER->setState(RUN);
-			PLAYER->setCenterX(PLAYER->getCenterX() + PLAYER->getSpeed());
 		}
+		if (INPUT->GetKey('D'))
+		{
+			if (GetPixel(IMAGEMANAGER->findImage("아침 집 충돌")->getMemDC(), PLAYER->getCenterX() + 8, PLAYER->getCenterY() + 8) != RGB(255, 0, 0))
+			{
+				if (!SOUNDMANAGER->isPlaySound("movewood"))
+				{
+					SOUNDMANAGER->play("movewood", 0.2f);
+				}
+				PLAYER->setDirection(RIGHT);
+				PLAYER->setState(RUN);
+				PLAYER->setCenterX(PLAYER->getCenterX() + PLAYER->getSpeed());
+			}
+		}
+
+		if (!(INPUT->GetKey('W')) && !(INPUT->GetKey('S')) && !(INPUT->GetKey('A')) && !(INPUT->GetKey('D')))
+		{
+			PLAYER->setState(STAND);
+		}
+
+
+
 	}
 
-	if (!(INPUT->GetKey('W')) && !(INPUT->GetKey('S')) && !(INPUT->GetKey('A')) && !(INPUT->GetKey('D')))
-	{
-		PLAYER->setState(STAND);
-	}
 
 }
 
