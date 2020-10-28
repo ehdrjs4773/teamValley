7bf29d274b46758fe82c7bfb966fafc722d4db72
Nia Kim <niakim63@gmail.com>
2020-10-28 16:32:00 +0900

    상점 인벤토리 수량 위치 조정중

--- 스타듀밸리 맵툴/shop.cpp
+++ 스타듀밸리 맵툴/shop.cpp
@@ -209,7 +209,20 @@ void shop::render()
 				{
 					char str[64];
 					wsprintf(str, "%d", _vInven->at(i).amount);
-					textOut(getMemDC(), _vInven->at(i).rc.left + 30, _vInven->at(i).rc.top + 30, str, RGB(0, 0, 0));
+					int left = 0;
+					if (_vInven->at(i).amount < 10)
+					{
+						left = 5;
+					}
+					else if (_vInven->at(i).amount >= 10 && _vInven->at(i).amount < 100)
+					{
+						left = 15;
+					}
+					else if (_vInven->at(i).amount >= 100)
+					{
+						left = 25;
+					}
+					textOut(getMemDC(), _vInven->at(i).rc.right - left, _vInven->at(i).rc.top + 30, str, RGB(0, 0, 0));
 				}
 			}
 		}
@@ -222,8 +235,20 @@ void shop::render()
 				{
 					char str[64];
 					wsprintf(str, "%d", _vInven->at(i).amount);
-
-					textOut(getMemDC(), _vInven->at(i).rc.left + 30, _vInven->at(i).rc.top + 30, str, RGB(0, 0, 0));
+					int left = 0;
+					if (_vInven->at(i).amount < 10)
+					{
+						left = 5;
+					}
+					else if (_vInven->at(i).amount >= 10 && _vInven->at(i).amount < 100)
+					{
+						left = 15;
+					}
+					else if (_vInven->at(i).amount >= 100)
+					{
+						left = 25;
+					}
+					textOut(getMemDC(), _vInven->at(i).rc.right - left, _vInven->at(i).rc.top + 30, str, RGB(0, 0, 0));
 				}
 			}
 		}
